8cec8d6963558336e3cdb25ca6a3048179280c99
Rodrigo Pombo <pombopombopombo@gmail.com>
2021-10-26 18:57:13 +0200

    Move demo page

--- pages/demo/[id].js
+++ pages/demo/[id].js
@@ -1,4 +1,18 @@
 import Head from "next/head";
+import { CodeHikeLogo } from "../../src/logo";
+import Link from "next/link";
+import React from "react";
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { id: "1" } },
+      { params: { id: "2" } },
+      { params: { id: "3" } },
+    ],
+    fallback: true,
+  };
+}
 
 export async function getStaticProps() {
   const mdxSource = mdx;
@@ -17,6 +31,7 @@ export async function getStaticProps() {
 }
 
 export default function Home({ sourceHtml }) {
+  const [showDemoMenu, setDemoMenu] = React.useState(false);
   return (
     <div className="flex flex-col min-h-screen">
       <Head>
@@ -27,8 +42,13 @@ export default function Home({ sourceHtml }) {
           rel="stylesheet"
         />
       </Head>
-      <nav className="flex w-full h-12 items-center gap-4 text-gray-700">
-        <h1 className="text-2xl font-bold pl-4">Code Hike</h1>
+      <nav className="flex w-full h-12 items-center gap-4 text-gray-800">
+        <Link href="/">
+          <a className="flex items-center gap-2 ml-4">
+            <CodeHikeLogo className="block h-7 w-7 text-blue-600" />
+            <h1 className="text-2xl font-bold">Code Hike</h1>
+          </a>
+        </Link>
         <div
           className="text-white h-10 p-4 pt-1 self-end rounded-tl-md rounded-tr-md"
           style={{ background: "#22272e" }}
@@ -37,7 +57,9 @@ export default function Home({ sourceHtml }) {
         </div>
         <div>styles.css</div>
         <div className="ml-auto">Theme</div>
-        <div className="pr-4">Examples</div>
+        <button className="pr-4" onClick={() => setDemoMenu(true)}>
+          Examples
+        </button>
       </nav>
       <main className="flex-1 flex" style={{ background: "#22272e" }}>
         <Source sourceHtml={sourceHtml} />
