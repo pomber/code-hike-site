ee189cb7f44bd8f76dfa2d43f1013b1f322e0fca
Rodrigo Pombo <pombopombopombo@gmail.com>
2021-11-20 18:25:39 +0100

    Use hover card

--- data/demos/custom-annotations/MyTooltipAnnotation.jsx
+++ data/demos/custom-annotations/MyTooltipAnnotation.jsx
@@ -1,8 +1,25 @@
+import * as HoverCard from "@radix-ui/react-hover-card";
+
 export function MyTooltipAnnotation({ children, data, theme }) {
   const border =
     typeof data === "string"
       ? data
       : theme.tokenColors.find((tc) => tc.scope?.includes("string"))?.settings
           ?.foreground || "yellow";
-  return <span style={{ outline: `2px solid ${border}` }}>{children}</span>;
+
+  return (
+    <HoverCard.Root openDelay={300}>
+      <HoverCard.Trigger asChild>
+        <span style={{ outline: `2px solid ${border}` }}>{children}</span>
+      </HoverCard.Trigger>
+      <HoverCard.Content
+        className="rounded w-72 p-4 bg-white shadow-lg unreset"
+        sideOffset={8}
+      >
+        <HoverCard.Arrow className="text-white" style={{ fill: "white" }} />
+        {data?.children || "Hey"}
+      </HoverCard.Content>
+    </HoverCard.Root>
+  );
+  return;
 }
