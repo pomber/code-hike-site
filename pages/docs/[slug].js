8a1176572946a7829d2b13410d775ac3afc7883d
Rodrigo Pombo <pombopombopombo@gmail.com>
2022-02-26 11:28:57 +0000

    Add experimental icon

--- pages/docs/[slug].js
+++ pages/docs/[slug].js
@@ -39,6 +39,16 @@ const section = [
   ["Troubleshooting", "troubleshooting"],
 ];
 
+const stable = [
+  "introduction",
+  "installation",
+  "configuration",
+  "codeblocks",
+  "troubleshooting",
+];
+function isExperimental(slug) {
+  return !stable.includes(slug);
+}
 export async function getStaticPaths() {
   return {
     paths: section.map(([title, slug]) => ({
@@ -214,11 +224,34 @@ function Sidebar({ current }) {
           }
         >
           <Link href={`/docs/${slug}`}>
-            <a className="block w-full select-none py-2 my-1 pl-2">{item}</a>
+            <a className="w-full select-none py-2 my-1 px-2 flex items-center">
+              <span className="flex-1">{item}</span>
+              {isExperimental(slug) && <ExperimentalIcon />}
+            </a>
           </Link>
         </li>
       ))}
     </ul>
+  );
+}
+
+function ExperimentalIcon() {
+  return (
+    <svg
+      className="w-4 h-4 block text-gray-500"
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <title>Experimental</title>
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z"
+      />
+    </svg>
   );
 }
 
