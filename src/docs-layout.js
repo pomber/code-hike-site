c02a085a13b3a188d385da60d00a25c253c59b3f
Rodrigo Pombo <pombopombopombo@gmail.com>
2022-08-08 13:31:58 +0000

    Add rows docs

--- src/docs-layout.js
+++ src/docs-layout.js
@@ -49,7 +49,7 @@ export function DocsLayout({ title, h1, slug, children, cardId, description }) {
                 <h1 className="text-2xl font-bold">
                   Code Hike
                   <span className="text-sm pl-2 font-normal text-gray-700">
-                    v0.7.1
+                    v0.7.3
                   </span>
                 </h1>
               </a>
