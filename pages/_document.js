428e8ec977f86cf590ebd6fc52efd8760275fe80
Rodrigo Pombo <pombopombopombo@gmail.com>
2024-02-20 17:50:48 +0100

    Add v1 banner

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,15 @@ class MyDocument extends Document {
       <Html lang="en" className="scroll-smooth">
         <Head />
         <body>
+          <div className="bg-blue-100 text-center py-1">
+            We are working on{" "}
+            <a
+              href="https://github.com/code-hike/codehike/discussions/413"
+              className="underline"
+            >
+              Code Hike v1.0
+            </a>
+          </div>
           <Main />
           <NextScript />
         </body>
